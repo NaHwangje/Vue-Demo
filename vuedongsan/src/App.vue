6a5962debdbf78c65a793882cb4fe8186253555b
NaHwangje <na03266@naver.com>
2023-09-06 09:17:08 +0900

    허위매물 신고 추가

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -4,13 +4,15 @@
       <a v-for="a in menu" :key="a"> {{ a }} </a>
     </div>
 
-    <div v-for="b in products" :key="b">
+    <div v-for="(b, i) in 3" :key="b">
       <img
         src="https://i.namu.wiki/i/MdIcQe5C3YFThD4FzDu9zoYYdRE4SpcGfb9cagDWVzn-a3cPR9TdMf1rF9Rk9zLDCZRZWD8ZIRKLi8c74CeLRPvaQNChqnrFtJ3uejTG5CvdHWX45xlEbgjQfGcC4VRgonFgHbRISx2bKOQiDjwang.webp"
         class="room-img"
       />
-      <h4>{{ b }}</h4>
-      <p>100 만원</p>
+      <h4>{{ products[i] }}</h4>
+      <p>{{ price[i] }} 만원</p>
+      <button @click="신고수++">허위매물 신고</button> <span>신고 수 : {{ 신고수 }} </span>
+      <!-- @ == v-on -->
     </div>
   </div>
 </template>
@@ -20,6 +22,7 @@ export default {
   name: "App",
   data() {
     return {
+      신고수 : 0,
       price: [60, 80, 90],
       products: ["상대동원룸", "하대동원룸", "상평동원룸"],
       menu: ["Home", "Shop", "About"],
