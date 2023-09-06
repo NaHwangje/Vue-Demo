e16cd4153d382a077272d3ecbcad013d978e9142
NaHwangje <na03266@naver.com>
2023-09-06 11:53:38 +0900

    사진 경로 배열에 담은 후에 자동으로 사진 넣기

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -4,14 +4,12 @@
       <a v-for="a in menu" :key="a"> {{ a }} </a>
     </div>
 
-    <div v-for="(b, i) in 3" :key="b">
-      <img
-        src="https://i.namu.wiki/i/MdIcQe5C3YFThD4FzDu9zoYYdRE4SpcGfb9cagDWVzn-a3cPR9TdMf1rF9Rk9zLDCZRZWD8ZIRKLi8c74CeLRPvaQNChqnrFtJ3uejTG5CvdHWX45xlEbgjQfGcC4VRgonFgHbRISx2bKOQiDjwang.webp"
-        class="room-img"
-      />
+    <div v-for="(b, i) in products" :key="b">
+      <img :src="imageUrls[i]" class="room-img" />
       <h4>{{ products[i] }}</h4>
       <p>{{ price[i] }} 만원</p>
-      <button @click="신고수++">허위매물 신고</button> <span>신고 수 : {{ 신고수 }} </span>
+      <button @click="신고수++">허위매물 신고</button>
+      <span>신고 수 : {{ 신고수 }} </span>
       <!-- @ == v-on -->
     </div>
   </div>
@@ -22,11 +20,42 @@ export default {
   name: "App",
   data() {
     return {
-      신고수 : 0,
+      신고수: 0,
       price: [60, 80, 90],
       products: ["상대동원룸", "하대동원룸", "상평동원룸"],
       menu: ["Home", "Shop", "About"],
+      imageUrls: [],
     };
+  },
+  created() {
+    // 이미지 폴더 내 모든 이미지 가져오기
+    const imageContext = require.context(
+      "./assets",
+      false,
+      /\.(png|jpe?g|gif|svg)$/
+    );
+
+    // 이미지 경로 배열 생성
+    this.imageUrls = imageContext.keys().map(imageContext);
+  },
+  methods: {
+    async loadImagesFromFolder(folderPath) {
+      // 폴더 내의 이미지 파일을 가져오기 위해 HTTP 요청을 보낼 수 있습니다.
+      // 예를 들어, 서버에서 해당 폴더 내의 이미지 목록을 제공하는 API를 호출할 수 있습니다.
+      try {
+        const response = await fetch(
+          `/api/get-images?folderPath=${folderPath}`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          this.imageUrls = data.imageUrls;
+        } else {
+          console.error("이미지 가져오기 실패");
+        }
+      } catch (error) {
+        console.error("이미지 가져오기 오류:", error);
+      }
+    },
   },
   components: {},
 };
@@ -34,9 +63,10 @@ export default {
 
 <style>
 .room-img {
-  width: 70%;
-  margin-top: 15px;
+  width: 100%;
+  margin-top: 40px;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
