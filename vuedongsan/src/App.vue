cc888e75b716aef20af68e7613b200879c1e327c
NaHwangje <na03266@naver.com>
2023-09-08 20:00:36 +0900

    원인 불명 오류 발생

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,91 +1,65 @@
 <template>
   <div>
-    <div class="black-bg" v-if="모달창열렸니 == true">
+    <!-- <div class="black-bg" v-if="모달창열렸니 == true">
       <div class="white-bg">
-        <h4>
-          상세 페이지 <span><button @click="모달창열렸니=false">x</button></span>
-        </h4>
+        <h4>상세 페이지</h4>
         <p>상세 페이지 내용임</p>
+        <button @click="모달창열렸니 = false">x</button>
       </div>
-    </div>
+    </div> -->
     <div class="menu">
       <a v-for="a in menu" :key="a"> {{ a }} </a>
     </div>
 
-    <div v-for="(b, i) in products" :key="b">
-      <img :src="imageUrls[i]" class="room-img" />
-      <h4 @click="모달창열렸니 = true">{{ products[i] }}</h4>
-      <p>{{ price[i] }} 만원</p>
-      <button @click="신고수[i]++">허위매물 신고</button>
-      <span>신고 수 : {{ 신고수[i] }} </span>
-      <!-- @ == v-on -->
+    <div v-for="(n,i) in onerooms " :key="n">
+      <img :src="onerooms[i].image" class="room-img" />
+
+      <h4 @click="모달창열렸니 = true">{{ onerooms[i].title }}</h4>
+
+      <p>{{ onerooms[i].price }} 만원</p>
+      
     </div>
   </div>
 </template>
- 
+
+
 <script>
+import oneroomsData from './assets/oneroom'
+
 export default {
   name: "App",
   data() {
     return {
-      모달창열렸니 : true,
-      신고수: [0,0,0],
-      price: [60, 80, 90],
-      products: ["상대동원룸", "하대동원룸", "상평동원룸"],
+     onerooms: oneroomsData,
+      모달창열렸니: false,
       menu: ["Home", "Shop", "About"],
-      imageUrls: [],
     };
   },
-  created() {
-    // 이미지 폴더 내 모든 이미지 가져오기
-    const imageContext = require.context(
-      "./assets",
-      false,
-      /\.(png|jpe?g|gif|svg)$/
-    );
-
-    // 이미지 경로 배열 생성
-    this.imageUrls = imageContext.keys().map(imageContext);
-  },
-  methods: {
-    async loadImagesFromFolder(folderPath) {
-      // 폴더 내의 이미지 파일을 가져오기 위해 HTTP 요청을 보낼 수 있습니다.
-      // 예를 들어, 서버에서 해당 폴더 내의 이미지 목록을 제공하는 API를 호출할 수 있습니다.
-      try {
-        const response = await fetch(
-          `/api/get-images?folderPath=${folderPath}`
-        );
-        if (response.ok) {
-          const data = await response.json();
-          this.imageUrls = data.imageUrls;
-        } else {
-          console.error("이미지 가져오기 실패");
-        }
-      } catch (error) {
-        console.error("이미지 가져오기 오류:", error);
-      }
-    },
-  },
+  created() {},
+  methods: {},
   components: {},
 };
 </script>
 
 <style>
-body{
+body {
   margin: 0;
 }
-div{
+div {
   box-sizing: border-box;
 }
-.black-bg{
-  width: 100%; height: 100%;
-  background: rgba(0,0,0,0.5);
-  position: fixed; padding: 20px;
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
 }
-.white-bg{
-  width: 100%; background: white;
-border-radius: 8px;  
-padding: 20px;
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 .room-img {
   width: 100%;
