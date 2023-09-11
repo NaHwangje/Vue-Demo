79dcb8d061a3b1f92082b7c080b20237f7d2b27f
NaHwangje <na03266@naver.com>
2023-09-11 16:09:27 +0900

    updatre

--- calculater/src/App.vue
+++ calculater/src/App.vue
@@ -1,12 +1,34 @@
 <script setup>
-import { reactive } from "vue";
+import { reactive, computed } from "vue";
 
 const data = reactive({
   number: 0,
+  temp: 0,
+  operator: null,
 });
 
 function inputNumber(number) {
-  data.number = number;
+  if (data.number == 0) data.number = number;
+  else data.number = data.number * 10 + number;
+}
+function inputOperator(value) {
+  data.temp = data.number;
+  data.number = 0;
+  data.operator = value;
+}
+const displayed = computed(() => {
+  let text = ''
+  if (data.temp) text += data.temp;
+  if (data.operator) text += data.operator;
+  if (data.number) text += data.number;
+  return text;
+});
+
+function claculate() {
+  let result = eval(displayed.value);
+  data.temp = 0
+  data.operator = null
+  data.number = result
 }
 </script>
 
@@ -15,18 +37,17 @@ function inputNumber(number) {
     <div
       class="col-span-4 border p-4 text-right rounded-lg bg-white text-black"
     >
-      display
+      {{ displayed }}
     </div>
     <div class="grid grid-cols-3 gap-4 col-span-3">
-      <button v-for="n of 9">{{ n }}</button>
-      <button>0</button>
-      <button class="col-span-2">=</button>
+      <button v-for="n of 9" @click="inputNumber(n)">{{ n }}</button>
+      <button @click="inputNumber(0)">0</button>
+      <button class="col-span-2" @click="claculate">=</button>
     </div>
     <div class="grid col-span-1 gap-4">
-      <button>+</button>
-      <button>-</button>
-      <button>*</button>
-      <button>/</button>
+      <button v-for="o of ['+', '-', '*', '/']" @click="inputOperator(o)">
+        {{ o }}
+      </button>
     </div>
   </div>
 </template>
