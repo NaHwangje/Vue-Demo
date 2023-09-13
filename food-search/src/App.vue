432266908d40418609dff752aad37fb282849e0a
NaHwangje <na03266@naver.com>
2023-09-13 17:39:34 +0900

    커밋 수정!

--- food-search/src/App.vue
+++ food-search/src/App.vue
@@ -1,44 +1,38 @@
 <script setup>
-import { reactive } from "vue";
+import { ref } from "vue";
 
-const data = reactive({
-  food: null,
-});
+const food = ref(null);
 
-function process(object) {
-  object.ingredients = [];
-  for (let i = 1; i <= 20; i++) {
-    if (object[`strIngredient${i}`]) {
-      let ingredient = {
-        name: object[`strIngredient${i}`],
-        measure: object[`strMeasure${i}`],
-      };
-      object.ingredients.push(ingredient);
-    } else {
-      break;
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+  .then((res) => res.json())
+  .then((json) => {
+    let data = json.meals[0];
+    data.ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+      if (data[`strIngredient${i}`]) {
+        let ingredient = {
+          name: data[`strIngredient${i}`],
+          measure: data[`strMeasure${i}`],
+        };
+        data.ingredients.push(ingredient);
+      } else break;
     }
-  }
-  return object;
-}
-
-fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-  .then((response) => response.json())
-  .then((json) => (data.food = process(json.meals[0])))
-  .catch((error) => console.log(error));
+    food.value = data;
+  });
 </script>
 
 <template>
   <div>
-    <div v-if="data.food" class="shadow bg-gray-100 text-black rounded-lg">
-      <img class="rounded-t-lg" :src="data.food.strMealThumb" alt="" />
-      <div class="text-4xl font-bold p-2 my-8">{{ data.food.strMeal }}</div>
-      <div class="p-4">
-        <div v-for="i of data.food.ingredients" class="grid grid-cols-2">
-          <div class="text-right pr-4">{{ i.name }}</div>
-          <div class="text-left pl-4">{{ i.measure }}</div>
-        </div>
+    <div>{{ food }}</div>
+    <div class="bg-white text-black shadow rounded-xl">
+      <img :src="food.strMealThumb" class="rounded-t-xl" alt="" />
+      <div>{{ food.strMeal }}</div>
+      <div v-for="i of food.ingredients">
+        <div>{{ i.name }}</div>
+        <div>{{ i.measure }}</div>
+        <!-- 수정 -->
       </div>
-      <div class="p-4">{{ data.food.strInstructions }}</div>
     </div>
   </div>
 </template>
