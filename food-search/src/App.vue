d62a32b6d46de0af973914b949f11bed207f2a4c
NaHwangje <na03266@naver.com>
2023-09-13 12:21:53 +0900

    사진, 이름 추가

--- food-search/src/App.vue
+++ food-search/src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
-let foods = [
-  10, 3, 4, 17, 8
-]
-foods= foods.map(x=> x*2)
+import { reactive } from "vue";
+
+const data = reactive({
+  food: null,
+});
+
+fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+  .then((response) => response.json())
+  .then((json) => (data.food = json.meals[0]))
+  .catch((error) => console.log(error));
 </script>
 
 <template>
-  <div class="text-2xl">
-    <div v-for="food of foods">{{ food }}</div>
+  <div>
+    <div class="shadow bg-gray-100 text-black rounded-lg">
+      <img class="rounded-t-lg" :src="data.food.strMealThumb" alt="" />
+      <div>{{ data.food.strMeal }}</div>
+    </div>
   </div>
 </template>
